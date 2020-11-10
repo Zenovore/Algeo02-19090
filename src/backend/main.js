d5fff504e44abaaf8e1406de11caf254bb4844c4
Josep Marcello <josep1403@students.itb.ac.id>
2020-11-10 09:00:47 +0700

    Refactoring src codes

--- src/backend/main.js
+++ src/backend/main.js
@@ -37,49 +37,49 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
-
-
-const filterfile = function(req, file, cb) {
+const filterfile = function (req, file, cb) {
   // Accept txt or html only
   if (!file.originalname.match(/\.(txt|html)$/i)) {
-      req.fileValidationError  = 'Punten ka, file .txt atau html aja ya';
-      return cb(new Error('Punten ka, file .txt atau html aja ya'), false);
+    req.fileValidationError = 'Punten ka, file .txt atau html aja ya';
+    return cb(new Error('Punten ka, file .txt atau html aja ya'), false);
   }
   cb(null, true);
 };
 
 const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-      let dir = "./uploads";
-      if (!fs.existsSync(dir)){
-          fs.mkdirSync(dir);
-      }
-      cb(null,dir)
+  destination: function (req, file, cb) {
+    let dir = './uploads';
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+    cb(null, dir);
   },
 
   // By default, multer removes file extensions so let's add them back
-  filename: function(req, file, cb) {
-      cb(null, file.originalname);
-  }
-})
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
 
-let upload = multer({storage : storage,fileFilter : filterfile}).array('files',10);
-app.post("/upload", (req,res)=>{
-  upload(req,res, (err) =>{
-      if (req.fileValidationError){
-          return res.send(req.fileValidationError);
-      }
-      else if (req.files){
-          console.log(req.files);
-          return res.send('Dah berhasil ya'+'\n<hr/><a href="./">Upload more files</a>');
-      }
-      else if (err instanceof multer.MulterError) {
-          return res.send(err);
-      }
-      else if (err) {
-          return res.send(err);
-      }
-  })
+let upload = multer({ storage: storage, fileFilter: filterfile }).array(
+  'files',
+  10
+);
+app.post('/upload', (req, res) => {
+  upload(req, res, (err) => {
+    if (req.fileValidationError) {
+      return res.send(req.fileValidationError);
+    } else if (req.files) {
+      console.log(req.files);
+      return res.send(
+        'Dah berhasil ya' + '\n<hr/><a href="./">Upload more files</a>'
+      );
+    } else if (err instanceof multer.MulterError) {
+      return res.send(err);
+    } else if (err) {
+      return res.send(err);
+    }
+  });
 });
 
 app.listen(serverConfig.PORT, () => {
